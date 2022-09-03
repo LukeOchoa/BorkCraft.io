d12325e1bd3b2781f36bcbdbeded0b28bfa21c05
LukeOchoa <luke.ochoa@gmail.com>
2022-09-03 03:35:56 +0000

    add Login feature, Index/IndexMut trait for LoginForm, ImageCache Struct

--- src/borkcraft/borkcraft_app.rs
+++ src/borkcraft/borkcraft_app.rs
@@ -1,15 +1,75 @@
+use crate::login::login_page::login;
 use eframe::egui;
+use egui_extras::RetainedImage;
+use std::{
+    collections::HashMap,
+    ops::{Index, IndexMut},
+    sync::{Arc, Mutex},
+};
+
+#[derive(Default)]
+pub struct ImageCache {
+    pub cache: HashMap<i32, Image>,
+}
+#[derive(Default)]
+pub struct Image {
+    pub name: String,
+    pub image: Option<RetainedImage>,
+    pub path: Option<String>, // path is a location(a file) to a user submitted image.
+}
+
+impl ImageCache {
+    pub fn default() -> Self {
+        let instance: Self = Default::default();
+        instance
+    }
+}
+
+#[derive(Default)]
+pub struct LoginForm {
+    pub username: String,
+    pub password: String,
+}
+impl Index<&'_ str> for LoginForm {
+    type Output = String;
+    fn index(&self, s: &str) -> &String {
+        match s {
+            "password" => &self.password,
+            "username" => &self.username,
+            _ => panic!("unknown field: {}", s),
+        }
+    }
+}
+impl IndexMut<&'_ str> for LoginForm {
+    fn index_mut(&mut self, s: &str) -> &mut String {
+        match s {
+            "password" => &mut self.password,
+            "username" => &mut self.username,
+            _ => panic!("unknown field: {}", s),
+        }
+    }
+}
+
+impl LoginForm {
+    pub fn get(&self, index: &str) -> Result<&String, String> {
+        match index {
+            "password" => Ok(&self.password),
+            "username" => Ok(&self.username),
+            _ => Err(format!("This struct member: |{}| does not exist", index)),
+        }
+    }
+}
 
 pub struct BorkCraft {
-    pub placeholder: String,
+    pub image_cache: Arc<Mutex<ImageCache>>,
+    pub login_form: LoginForm,
 }
 
 impl Default for BorkCraft {
     fn default() -> Self {
-        let placeholder_string = String::from("Just a placeholder");
-
         Self {
-            placeholder: placeholder_string,
+            image_cache: Arc::new(Mutex::new(ImageCache::default())),
+            login_form: LoginForm::default(),
         }
     }
 }
@@ -17,7 +77,7 @@ impl Default for BorkCraft {
 impl eframe::App for BorkCraft {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(&self.placeholder);
+            login(self, ui);
         });
     }
 }
--- src/login/login_page.rs
+++ src/login/login_page.rs
@@ -1,1 +1,14 @@
-pub fn _login() {}
+use crate::borkcraft_app::BorkCraft;
+use eframe::egui;
+
+const LOGIN_FORM: &'static [&'static str] = &["username", "password"];
+
+pub fn login(the_self: &mut BorkCraft, ui: &mut egui::Ui) {
+    egui::Grid::new(1).show(ui, |ui| {
+        for item in LOGIN_FORM.iter() {
+            ui.label(item.clone());
+            ui.add(egui::TextEdit::singleline(&mut the_self.login_form[item]));
+            ui.end_row();
+        }
+    });
+}
